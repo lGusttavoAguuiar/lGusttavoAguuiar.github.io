105b00ca0e2c5f61f4c2cddafe480b20c4fca682
GUSTAVO CARLOS AGUIAR <gustavo.aguiar2@alunos.sc.senac.br>
2025-08-04 20:45:18 -0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>My first page in GitHub Pages - Gustavo Aguiar</h2>
+      <h3>Um texto legal</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
